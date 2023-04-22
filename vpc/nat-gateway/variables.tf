a9629821440a8429bdb1537442a641937256c4d3
ikechukwu Anthony <ikechukwu980@gmail.com>
2023-04-22 15:07:40 -0500

    added nat-gateway module

---
+++ vpc/nat-gateway/variables.tf
@@ -0,0 +1,13 @@
+# Nat gateway variables
+
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1.id" {}
+variable "internet_gateway" {}
+variable "public_subnet_az2.id" {}
+variable "vpc_id" {}
+variable "private_app_subnet_az1.id " {}
+variable "private_data_subnet_az1.id " {}
+variable "private_app_subnet_az2.id " {}
+variable "private_data_subnet_az2.id " {}
+
