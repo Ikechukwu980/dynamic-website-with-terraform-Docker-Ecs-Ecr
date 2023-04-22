d442978088984337821cbd0655ef7bb2f3fc5de6
ikechukwu Anthony <ikechukwu980@gmail.com>
2023-04-22 16:31:15 -0500

    new

--- nat-gateway/variables.tf
+++ nat-gateway/variables.tf
@@ -6,9 +6,8 @@ variable "public_subnet_az1_id" {}
 variable "internet_gateway" {}
 variable "public_subnet_az2_id" {}
 variable "vpc_id" {}
-variable "private_app_subnet_az1_cidr" {}
-variable "private_app_subnet_az2_cidr" {}
-variable "private_data_subnet_az1_cidr" {}
-variable "private_data_subnet_az2_cidr" {}
-
-
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
