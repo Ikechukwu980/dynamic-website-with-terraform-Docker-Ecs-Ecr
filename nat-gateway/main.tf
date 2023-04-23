2820dddaebf1b48132c46aaac232956b5c37fa52
ikechukwu Anthony <ikechukwu980@gmail.com>
2023-04-23 11:16:36 -0500

    added security group

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -71,10 +71,10 @@ resource "aws_route_table_association" "private_data_subnet_az1_rt_az1_associati
 
 # create private route table az2 and add route through nat gateway az2
 resource "aws_route_table" "private_route_table_az2" {
-  vpc_id = true
+  vpc_id = var.vpc_id
 
   route {
-    cidr_block     = "0.0.0.0./0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway_az2.id 
   }
 
---
+++ security-group/variables.tf
@@ -0,0 +1,5 @@
+# security group variable
+variable "project_name" {}
+variable "environment" {}
+variable "vpc_id" {}
+variable "ssh_ip" {}
